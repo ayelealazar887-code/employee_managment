6075688e75725eb6865891b0dd1c2677bb61d480
ayelealazar887-code <ayelealazar887@gmail.com>
2026-07-16 20:41:17 +0300

    Side bar UI completed

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Sidebar from '../components/Sidebar'
 
 function Layout() {
   return (
-    <div className='flex h-screen w-screen overflow-hidden'>
-      {/* Sidebar - fixed width */}
-      <aside className='w-64 flex-shrink-0 border-r border-gray-200 p-4'>
-        <p>Sidebar</p>
-      </aside>
-      
-      {/* Main Content Area */}
-      <main className='flex-1 h-full overflow-y-auto bg-gray-50 p-6'>
-        <div className='max-w-7xl mx-auto'>
+    <div className='min-h-screen bg-slate-100'>
+      <Sidebar />
+
+      <main className='min-h-screen overflow-y-auto bg-slate-50 p-4 pt-16 md:ml-72 md:p-8 lg:p-10'>
+        <div className='mx-auto max-w-7xl'>
           <Outlet />
         </div>
       </main>
